ab0416e4fd8f8b76907cb40d2045ec864fe1702d
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-03-11 17:34:08 +0300

    C Basics - C Comment

---
+++ C Basics/03_c_comments.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    // This is a single-line comment
+    printf("Single-line Comment\n");
+
+    /* This is a mult-line
+        comment
+    */
+    printf("Multi-line Comment\n");
+    return 0;
+}
